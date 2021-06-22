5bdf0763b755448836a11609c11b8247ec0ba162
hugo.faye <hugo.faye@adevinta.com>
2021-06-22 20:05:45 +0200

    WIP : home nav grid

---
+++ components/ui/HomeNavGrid/HomeNavGrid.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import ImageButton from '../ImageButton/ImageButton'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const links = [
+  {
+    url: '/decor.jpeg',
+    title: 'SHOP',
+    destination: 'shop',
+  },
+  {
+    url: '/lampes.jpeg',
+    title: 'ASSEMBLAGE',
+    destination: 'assemblage',
+  },
+  {
+    url: '/maro3.jpeg',
+    title: 'ART',
+    destination: 'art',
+  },
+]
+
+export default function HomeNavGrid() {
+  return (
+    <Grid container spacing={2}>
+      {links.map((link: any) => (
+        <Grid item xs={12} sm={6} alignContent="center">
+          <ImageButton link={link} />
+        </Grid>
+      ))}
+    </Grid>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import { getConfig } from '@framework/api'
 import getAllProducts from '@framework/product/get-all-products'
 import getSiteInfo from '@framework/common/get-site-info'
 import getAllPages from '@framework/common/get-all-pages'
+import React from 'react'
+import HomeNavGrid from '@components/ui/HomeNavGrid/HomeNavGrid'
 
 export async function getStaticProps({
   preview,
@@ -42,71 +44,7 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      <Grid>
-        {products.slice(0, 3).map((product, i) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            imgProps={{
-              width: i === 0 ? 1080 : 540,
-              height: i === 0 ? 1080 : 540,
-            }}
-          />
-        ))}
-      </Grid>
-      <Marquee variant="secondary">
-        {products.slice(0, 3).map((product, i) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            variant="slim"
-            imgProps={{
-              width: 320,
-              height: 320,
-            }}
-          />
-        ))}
-      </Marquee>
-      <Hero
-        headline="Release Details: The Yeezy BOOST 350 V2 ‘Natural'"
-        description="
-        The Yeezy BOOST 350 V2 lineup continues to grow. We recently had the
-        ‘Carbon’ iteration, and now release details have been locked in for
-        this ‘Natural’ joint. Revealed by Yeezy Mafia earlier this year, the
-        shoe was originally called ‘Abez’, which translated to ‘Tin’ in
-        Hebrew. It’s now undergone a name change, and will be referred to as
-        ‘Natural’."
-      />
-      <Grid layout="B">
-        {products.slice(0, 3).map((product, i) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            imgProps={{
-              width: i === 0 ? 1080 : 540,
-              height: i === 0 ? 1080 : 540,
-            }}
-          />
-        ))}
-      </Grid>
-      <Marquee>
-        {products.slice(0, 3).map((product, i) => (
-          <ProductCard
-            key={product.id}
-            product={product}
-            variant="slim"
-            imgProps={{
-              width: 320,
-              height: 320,
-            }}
-          />
-        ))}
-      </Marquee>
-      {/* <HomeAllProductsGrid
-        newestProducts={products}
-        categories={categories}
-        brands={brands}
-      /> */}
+      <HomeNavGrid />
     </>
   )
 }
